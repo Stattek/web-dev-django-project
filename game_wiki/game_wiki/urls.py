c040b25028757bd5dd85ba66225072e16886a31a
stattek <alienhuntsgames@gmail.com>
2024-12-04 17:14:42 -0600

    Fixed path.
    - Made main page show up at the base URL by changing the entry app to be
    the main app.

--- game_wiki/game_wiki/urls.py
+++ game_wiki/game_wiki/urls.py
@@ -22,7 +22,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("entry/", include("entry.urls")),
+    path("", include("entry.urls")),
     path('armory/', include('Armory.urls'))
 ]
 
